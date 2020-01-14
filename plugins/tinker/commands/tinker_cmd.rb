30c58db141df10a3bbf5420bece61957d9166ec8
Test MU Admin <gimmisky@gmail.com>
2020-01-14 06:22:40 +0000

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -2,15 +2,20 @@ module AresMUSH
   module Tinker
     class TinkerCmd
       include CommandHandler
-
+      
       def check_can_manage
         return t('dispatcher.not_allowed') if !enactor.has_permission?("tinker")
         return nil
       end
-
-      def handle
-        client.emit_success "REALLY Done!"
-      end
+      
+    def handle
+        char = Character.all.select { |c| c.name == cmd.args }.first
+            if (char)
+                client.emit "You found #{char.name}"
+            else
+                client.emit "Nothing found."
+    end
+end
 
     end
   end
