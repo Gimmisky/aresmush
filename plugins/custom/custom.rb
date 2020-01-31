4901c35d90835d212b91674d6d17730fe9e92a86
Gimmisky <50979911+Gimmisky@users.noreply.github.com>
2020-01-31 15:07:01 -0300

    Update custom.rb

--- plugins/custom/custom.rb
+++ plugins/custom/custom.rb
@@ -16,6 +16,19 @@ module AresMUSH
         case cmd.switch
         when "set"
           return SetMotivationsCmd
+        else
+          return MotivationsCmd
+        end
+      end
+      return nil
+    end
+
+    def self.get_cmd_handler(client, cmd, enactor)
+      case cmd.root
+      when "motivations"
+        case cmd.switch
+        when "set"
+          return SetMotivationsCmd
         end
       end
       return nil
